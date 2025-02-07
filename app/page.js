44ba7c8e06f4268754d6069fe7d3d13ef88e98d0
Wilsonli33092 <90936549+WilsonLi33092@users.noreply.github.com>
2025-02-07 15:12:13 -0500

    did some more text updates

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <p>
-        Hello this is testing a new deploy
-      </p>
+    <div className = "m-20">
+      <p className = "text-xl text-blue-500">Check to see if this deployed correctly</p>
+      <p className = "m-20 text-green-500">Another update that i have added here to see if it deploys</p>
+      <p className = "text-xl text-blue-500 ml-40">Wow it's like cascading</p>
+      <p className = "text-xl text-red-500 ml-60 mt-20">We can do different sizes too</p>
     </div>
   );
 }
